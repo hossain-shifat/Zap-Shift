5e2b42c27f1c93090c28aa1ac162e89c40eb43db
Hossain-Shifat <161940464+hossain-shifat@users.noreply.github.com>
2025-11-18 10:20:20 +0600

    Add firebase

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div>
-            this is about page
+        <div className="p-5 md:p-10 border border-base-100 shadow-lg rounded-2xl bg-white">
+            <div className="space-y-6">
+                <div className="space-y-2">
+                    <h1 className="font-bold text-2xl md:text-4xl">About Us</h1>
+                    <p className="max-w-2xl">Enjoy fast, reliable parcel delivery with real-time tracking and zero hassle. From personal packages to business shipments — we deliver on time, every time.</p>
+                </div>
+                <div className="space-y-4">
+                    <div>
+                        <h1 className="font-bold text-xl">We deliver almost all over Bangladesh</h1>
+                    </div>
+                    <div className="w-full h-[500px]">
+
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/pages/Auth/Register/Register.jsx
+++ src/pages/Auth/Register/Register.jsx
@@ -2,48 +2,67 @@ import { ArrowUp, Eye, EyeClosed, User } from 'lucide-react'
 import React, { useState } from 'react'
 import { assets } from '../../../assets/assets'
 import { Link } from 'react-router'
+import { useForm } from 'react-hook-form'
+import { register } from 'swiper/element'
 
 const Register = () => {
+
     const [showPassword, setShowPassword] = useState(false)
+    const { register, handleSubmit, formState: { errors } } = useForm()
+
+    const handleRegistration = (data) => {
+        console.log(data)
+    }
+
+
     return (
-        <div className="max-w-full mx-auto md:mx-15">
-            <div class="w-full max-w-[450px] p-7 rounded-2xl">
-                <form>
+        <div className="max-w-full mx-auto md:mx-15 md:-mt-5">
+            <div className="w-full max-w-[450px] p-7 rounded-2xl">
+                <form onSubmit={handleSubmit(handleRegistration)} className="space-y-3">
                     <div className="space-y-1">
                         <h1 className="font-bold text-4xl">Create an Account</h1>
                         <p className="">Register with ZapShift</p>
                     </div>
-                    <div className="my-4">
+                    {/* image upload feild */}
+                    <div className="w-16 h-16">
                         <label className="cursor-pointer">
                             <div className="relative flex justify-center items-center w-15 h-15">
                                 <User size={40} className="" fill='gray' stroke='gray' />
-                                <ArrowUp size={25} className="absolute text-primary w-7 h-7 rounded-full bg-white -right-2 bottom-0"/>
+                                <ArrowUp size={25} className="absolute text-primary  rounded-full bg-white -right-2 bottom-0" />
                             </div>
                             <input type="file" name='file' className="hidden" />
                         </label>
                     </div>
-                    <div className="my-4">
+                    {/* name feild */}
+                    <div>
                         <label className="text-lg">Name</label>
-                        <input type="text" placeholder='Name' name='name' className="w-full p-3 bg-base-100 rounded-xl border focus-within:outline outline-[#94A3B8] placeholder:text-[#94A3B8] text-base-content text-md" />
+                        <input type="text" placeholder='Name' {...register('name', { required: true })} className="w-full p-3 bg-base-100 rounded-xl border focus-within:outline outline-[#94A3B8] placeholder:text-[#94A3B8] text-base-content text-md" />
+                        {errors.name?.type === 'required' && <p className="text-red-500">Name is Required!</p>}
                     </div>
-                    <div className="my-4">
+                    {/* email feild */}
+                    <div>
                         <label className="text-lg">Email</label>
-                        <input type="email" placeholder='Email' name='email' className="w-full p-3 bg-base-100 rounded-xl border focus-within:outline outline-[#94A3B8] placeholder:text-[#94A3B8] text-base-content text-md" />
+                        <input type="email" placeholder='Email' {...register('email', { required: true })} className="w-full p-3 bg-base-100 rounded-xl border focus-within:outline outline-[#94A3B8] placeholder:text-[#94A3B8] text-base-content text-md" />
+                        {errors.email?.type === 'required' && <p className="text-red-500">Email is Required!</p>}
                     </div>
-                    <div className="my-4">
+                    {/* password feild */}
+                    <div>
                         <label>Password</label>
                         <div className="relative">
-                            <input type={showPassword ? 'text' : 'password'} placeholder='Password' name='password' className="w-full p-3 bg-base-100 rounded-xl border focus-within:outline outline-[#94A3B8] placeholder:text-[#94A3B8] text-base-content text-md" />
+                            <input type={showPassword ? 'text' : 'password'} placeholder='Password' {...register('password', { required: true, minLength: 6, pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/ })} className="w-full p-3 bg-base-100 rounded-xl border focus-within:outline outline-[#94A3B8] placeholder:text-[#94A3B8] text-base-content text-md" />
                             <span onClick={() => setShowPassword(!showPassword)} className="absolute right-4 top-3 cursor-pointer">{showPassword ? <EyeClosed /> : <Eye />}</span>
+                            {errors.password?.type === 'required' && <p className="text-red-500">Password is Required!</p>}
+                            {errors.password?.type === 'minLength' && <p className="text-red-500">Password must be 6 characters or longer</p>}
+                            {errors.password?.type === 'pattern' && <p className="text-red-500">Password must have an uppercase, at least one lowercase, atleast one number and at least one special character</p>}
                         </div>
                     </div>
-                    <div className="w-full my-4">
+                    <div className="w-full my-2">
                         <button className="btn btn-primary w-full text-black font-bold text-[1.1rem] rounded-xl">Register</button>
                     </div>
                     <p>Already have an account? <Link to='/login' className="hover:underline text-primary cursor-pointer">Login</Link></p>
                 </form>
                 <div>
-                    <div className="flex justify-center items-center gap-2 my-2">
+                    <div className="flex justify-center items-center gap-2 my-1">
                         <hr className="w-[45%]" />
                         <h1>Or</h1>
                         <hr className="w-[45%]" />
